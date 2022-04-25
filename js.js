2d89a35526aa8f43bfb32a380018f6aa92f19bec
Nan Wang <100560300+wanny017@users.noreply.github.com>
2022-04-25 17:17:57 +0930

    Update js.js

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 //current date
 var today = moment();
-$("#currentDay").text(today.format("dddd, MMM Do, YYYY, hh:mm "));
+$("#currentDay").text(today.format("dddd, MMM Do, YYYY, hh:mm A"));
 //creat input boxes
 var time = ['09:00 AM','10:00 AM','11:00 AM','12:00 AM','01:00 PM','02:00 PM','03:00 PM','04:00 PM','05:00 PM',];
 
